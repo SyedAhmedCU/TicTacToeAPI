ad70d5cf4d91f6bbbdf4fa1fdf5291d1987666e8
Syed Tasnim Ahmed <syedtasnim.iutoic@gmail.com>
2022-11-17 08:55:41 -0500

    Add move controller with dependency injection

---
+++ TicTacToeAPI/Controllers/MoveController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicTacToeAPI.Data;
+
+namespace TicTacToeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoveController : ControllerBase
+    {
+        //Dependency injection for in memory database context
+        private readonly TicTacToeAPIDbContext dbContext;
+        public MoveController(TicTacToeAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
