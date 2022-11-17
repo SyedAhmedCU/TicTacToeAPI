afe8e27381f20bd230621c53d03144bed23bad9c
Syed Tasnim Ahmed <syedtasnim.iutoic@gmail.com>
2022-11-16 20:38:19 -0500

    Add unit test for GetCurrentGames endpoint

---
+++ TicTacToeAPI.UnitTests/Systems/Controllers/TestGameController.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeAPI.Controllers;
+using TicTacToeAPI.Data;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace TicTacToeAPI.UnitTests.Systems.Controllers
+{
+    public class TestGameController
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReturnStatusCode200()
+        {
+            //add in memory database context
+            var optionsBuilder = new DbContextOptionsBuilder<TicTacToeAPIDbContext>()
+                .UseInMemoryDatabase("GameDb");
+            var dbContext = new TicTacToeAPIDbContext(optionsBuilder.Options);
+            //Arrange
+            var sut = new GameController(dbContext);
+
+            //Act
+            var result = (OkObjectResult)await sut.GetCurrentGames();
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
