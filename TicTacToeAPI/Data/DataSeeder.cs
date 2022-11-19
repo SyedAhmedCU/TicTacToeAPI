732148a62370243f08158413e535b74cf8dbaafe
Syed Tasnim Ahmed <syedtasnim.iutoic@gmail.com>
2022-11-18 20:11:25 -0500

    Add and setup data seeding with 4 players data

---
+++ TicTacToeAPI/Data/DataSeeder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using TicTacToeAPI.Data;
+using TicTacToeAPI.Model;
+
+namespace TicTacToeAPI.Data
+{
+    public class DataSeeder
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var gameContext = serviceScope.ServiceProvider.GetService<TicTacToeAPIDbContext>();
+                if (!gameContext.Players.Any())
+                {
+                    var players = new List<Player>()
+                    {
+                        new Player {
+                            Name = "Michael"
+                        },
+                        new Player {
+                            Name = "Toby"
+                        },
+                        new Player {
+                            Name = "Pam"
+                        },
+                        new Player {
+                            Name = "Jim"
+                        }
+                    };
+                    gameContext.Players.AddRange(players);
+                    gameContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
